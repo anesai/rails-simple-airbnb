2dafefcc889dd652a72639a8762449263979b2b1
Akane Saito <ukku19@gmail.com>
2023-09-14 12:16:04 +0200

    worked on views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def guests_icon
+    '<i class="fas fa-user"></i>'.html_safe
+  end
+
+  def british_pound_icon
+    '<i class="fa-solid fa-sterling-sign"></i>'.html_safe
+  end
 end
