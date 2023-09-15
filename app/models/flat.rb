3144049f47500c3cfe79eea825c8aabe97301ca2
Akane Saito <ukku19@gmail.com>
2023-09-15 13:27:17 +0200

    navbar

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,6 +2,6 @@ class Flat < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :description, presence: true, length: { minimum: 50 }
-  validates :price_per_night, presence: true, numericality: { only_integer: true }
-  validates :number_of_guests, presence: true, numericality: { only_integer: true }
+  validates :price_per_night, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :number_of_guests, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
